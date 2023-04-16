02e169ef3f9d5bc1390125ba379a26161b6adefc
Andreas Kasper <djassi@web.de>
2023-04-16 00:25:34 +0000

    indexer fertig für md5

--- src/html/app/code/classes/bots/indexer.php
+++ src/html/app/code/classes/bots/indexer.php
@@ -4,10 +4,15 @@ namespace bots;
 
 class indexer {
 
+    public static $json_index = [];
+
     public static function start() {
         if (!file_exists("/in/")) die("[ERROR] /in/ Verzeichnis fehlt!".PHP_EOL);
         if (!file_exists("/data/")) mkdir("/data/");
         if (!file_exists("/out/")) mkdir("/out/");
+
+        if (!file_exists("/data/index.json")) file_put_contents("/data/index.json","[]");
+        self::$json_index = json_decode(file_get_contents("/data/index.json"),true);
 
         $dirs = array("/in/");
         $i = 0;
@@ -22,14 +27,31 @@ class indexer {
                 echo("[💾] ".$dir.$file.PHP_EOL);
                 $pi = pathinfo($dir.$file);
                 if (!self::is_videofile($pi)) continue;
-                $md5 = md5_file($dir.$file);
-                echo($md5.PHP_EOL);
+                $file2 = substr($dir.$file, 3, 9999);
+                $row = self::indexdata_by_filename($file2);
+                if ($row == false) {
+                    $md5 = md5_file($dir.$file);
+                    self::$json_index[] = array(
+                        "filename" => $file2,
+                        "filesize" => filesize($dir.$file),
+                        "md5" => $md5
+                    );
+                    file_put_contents("/data/index.json", json_encode(self::$json_index));
+                }
             }
         }
     }
 
     public static function is_videofile(Array $arr) : bool {
         if (strtolower($arr["extension"] ?? "") == "mp4") return true;
+        return false;
+    }
+
+    public static function indexdata_by_filename($filename) : Array|false {
+        for ($i = 0; $i < count(self::$json_index); $i++) {
+            $row = self::$json_index[$i];
+            if ($row["filename"] == $filename) return array($row, $i);
+        }
         return false;
     }
 
